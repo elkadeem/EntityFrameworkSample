a07e1588e886d61900397afbb4f6fbfa9c503dea
Wael Elkadim <elkadeem@hotmail.com>
2020-09-07 14:44:20 +0200

    Add project files.

---
+++ EntityFrameworkSample/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Linq;
+
+namespace EntityFrameworkSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var dbContext = new TestDbContext())
+            {
+                if (!dbContext.Customers.Any(c => c.Code == "1"))
+                {
+                    dbContext.Customers.Add(new Customer
+                    {
+                        Code = "1",
+                        Name = "Customer 1",
+                    });
+
+                    dbContext.SaveChanges();
+                }
+
+                AddDocument(dbContext, 1);
+            }
+
+            new Program().AddDocument(null, 0);
+            Console.ReadLine();
+        }
+
+        private void AddDocument(TestDbContext dbContext, int customerId)
+        {
+
+            using (var transaction = dbContext.Database.BeginTransaction())
+            {
+                try
+                {
+                    Document document = CreateDocument(customerId);
+                    dbContext.Documents.Add(document);
+                    dbContext.SaveChanges();
+
+                    AddItems(document);
+                    document.Total = document.Items.Sum(c => c.TotalPrice);
+                    document.Items.Last().Name = null;
+
+                    dbContext.SaveChanges();
+                    transaction.Commit();
+
+                    Console.WriteLine($"Add Document with Id {document.Id}");
+                }
+                catch
+                {
+                    transaction.Rollback();
+                }
+            }
+        }
+
+        private static void AddItems(Document document)
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                document.Items.Add(new DocumentItem
+                {
+                    Name = $"Item {i}",
+                    Quantity = 10 * i,
+                    UnitPrice = 1,
+                });
+            }
+
+            foreach (var item in document.Items)
+            {
+                item.TotalPrice = item.Quantity * item.UnitPrice;
+            }
+        }
+
+        private static Document CreateDocument(int customerId)
+        {
+            return new Document()
+            {
+                CreationDate = DateTime.Now,
+                CustomerId = customerId,
+                DocumentDate = DateTime.Now,
+                DocumentNumber = "1",
+            };
+        }
+
+        private static void AddDocument(TestDbContext dbContext, int customerId)
+        {
+            using (var transaction = dbContext.Database.BeginTransaction())
+            {
+                try
+                {
+                    Document document = new Document()
+                    {
+                        CreationDate = DateTime.Now,
+                        CustomerId = customerId,
+                        DocumentDate = DateTime.Now,
+                        DocumentNumber = "1",
+                    };
+
+                    dbContext.Documents.Add(document);
+                    dbContext.SaveChanges();
+
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        document.Items.Add(new DocumentItem
+                        {
+                            Name = $"Item {i}",
+                            Quantity = 10 * i,
+                            UnitPrice = 1,
+                        });
+                    }
+
+                    foreach (var item in document.Items)
+                    {
+                        item.TotalPrice = item.Quantity * item.UnitPrice;
+                    }
+
+                    document.Total = document.Items.Sum(c => c.TotalPrice);
+
+                   
+                    document.Items.Last().Name = null;
+                    dbContext.SaveChanges();
+                    transaction.Commit();
+
+                    Console.WriteLine($"Add Document with Id {document.Id}");
+                }
+                catch
+                {
+                    transaction.Rollback();                   
+                }
+            }
+        }
+    }
+}
---
+++ EntityFrameworkSample/TestDbContext.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkSample
+{
+    public class TestDbContext : DbContext
+    {
+        public TestDbContext() 
+            : this(ConfigurationManager.ConnectionStrings["TestConnectionString"].ConnectionString)
+        {
+            //Database.SetInitializer<TestDbContext>(new CreateDatabaseIfNotExists<TestDbContext>());
+            Database.SetInitializer<TestDbContext>(null);
+        }
+        public TestDbContext(string connectionString) : base(connectionString)
+        { 
+
+        }
+
+        public DbSet<Document> Documents { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Document>()
+            //    .Property(c => c.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+
+    }
+
+
+    public class Document
+    {
+        public Document()
+        {
+            Items = new HashSet<DocumentItem>();
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime DocumentDate { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string DocumentNumber { get; set; }
+
+        public double Total { get; set; }
+        
+        public DateTime CreationDate { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public ICollection<DocumentItem> Items { get; set; }
+    }
+
+    public class DocumentItem
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+        
+        public int DocumentId { get; set; }
+
+        //[Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public double Quantity { get; set; }
+
+        public double UnitPrice { get; set; }
+
+        public double TotalPrice { get; set; }
+
+    }
+
+    public class Customer
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string Code { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+    }
+}
